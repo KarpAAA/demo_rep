69b33f7d38fc89d1e5ad919aba096d4b1b7b5e86
KarpAAA <110966872+KarpAAA@users.noreply.github.com>
2022-11-18 10:26:20 +0200

    Update codeTask.cpp
    11

--- codeTask.cpp
+++ codeTask.cpp
@@ -1,99 +1,137 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
 #include <iostream>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <string>
+#include <cstring>
 #include <cmath>
+#include <unistd.h>
 
-
-//4. Табулювати функцію sin x, задану розкладом в ряд Тейлора, в області
-//її визначення на відрізку від А до В(кількість кроків не менше 100
-//000 –задається користувачем).
-
-const double DEFAULT_A = 0;
-const double DEFAULT_B = 1000;
-const double DEFAULT_STEP = 0.001;
-
-double factorial(double i);
+using namespace std;
+void printingTable();
+int changeAvailibility();
+std::string task3();
 double radians(double degrees);
-double power(double x, double n);
-double sinus(double radians);
 
+int main() {
 
-int main(int argc, char** argv) {
-
-	
-	double A = std::atof(argv[1]);
-	double B = std::atof(argv[2]);
-	double h = std::atof(argv[3]);
-
-
-	if (B < A || B<0 || A<0){
-		B = DEFAULT_B;
-		A = DEFAULT_A;
-		if ((B - A) / h < 100000) h = (B-A)/100000;
+	std::string result = "";
+	int myFile;
+	if ((myFile = creat("./lab8_Output.txt", S_IRWXU)) < 0)
+	{
+		cout << "File was not created\n";
 	}
-	else {
-	
-		
-		if ((B - A) / h < 100000) {
-			B = DEFAULT_B;
-			A = DEFAULT_A;
-			h = DEFAULT_STEP;
+
+
+	while (true) {
+		printingTable();
+
+		char actionNumber;
+
+		cin >> actionNumber;
+		bool finish = false;
+
+		switch (actionNumber)
+		{
+		case '1':
+		{
+			result = "1\n";
+			system("cls");
+
+			write(myFile, result.c_str(), strlen(result.c_str()));
+			break;
 		}
-		
+		case '2':
+		{
+			system("cls");
+			int res = changeAvailibility();
+			system("cls");
+			if (res == 1) {
+				
+				int switchMode;
+				if ((switchMode = chmod("./lab8_result.txt", S_IREAD)))
+				{
+					cout << "Read mode enabled" << endl;
+				}
+				
+			}
+			else if (res == 2) {
+				
+
+				int switchMode;
+				if ((switchMode = chmod("./lab8_result.txt", S_IREAD)))
+				{
+					cout << "Read mode enabled" << endl;
+				}
+
+			}
+			else {}
+			break;
+		}
+
+		case '0':
+		{
+			finish = true;
+			break;
+		}
+		default:
+			break;
+		}
+		if (finish) break;
+
 	}
-	
-	std::cout << A << " " << B << " " << h << std::endl;
-	std::cout << "Press any key to continue";
+	close(myFile);
+
 	getchar();
-
-	double value = 0;
-	for (double i = A; i <= 1000; i+=h) {
-
-		value = 0;
-		
-
-		value = sinus(radians(i));
-		std::cout << "Sin( " << i << " ) = ";
-		std::cout << value << std::endl;
-	
-	}
-
-
 	return 0;
 }
-double factorial(double i)
-{
-	if (i == 0) return 1;
-	else return i * factorial(i - 1);
-	
-}
+void printingTable() {
 
-double radians(double degrees)  
+	cout << "\n---------------------------------------------" << endl;
+
+	cout << "\n(1) - to write 3laba result to file\n(2) - to change file availability\n\n(0) - to EXIT";
+
+	cout << "\n---------------------------------------------" << endl;
+}
+int changeAvailibility() {
+	int actionNumber;
+
+	cout << "\n(1) - to ONLY_READ mode\n(2) - to WRITE mode\n\n(0) - to EXIT";
+	cin >> actionNumber;
+	return actionNumber;
+}
+std::string task3() {
+	std::string result = "";
+	double A = 1;
+	double B = 1000;
+	double h = 0.01;
+
+	double value = 0;
+	for (double i = A; i <= B; i += h) {
+
+		value = sin(radians(i));
+		result += "Sin( ";
+		result += (std::to_string(i));
+		result += " ) = ";
+		result += (std::to_string(value));
+		result += "\n";
+
+	}
+	return result;
+}
+double radians(double degrees)
 {
 	double radians;
 	double const pi = 3.14159265358979323846;
 	radians = (pi / 180) * degrees;
 	return radians;
-}
-
-double power(double x, double n) 
-{
-	double output = 1;
-	while (n > 0)
-	{
-		output = (x * output);
-		n--;
-	}
-	return output;
-}
-
-double sinus(double radians){
-	double a, b, c;
-	float result = 0;
-	for (int y = 0; y != 9; y++)
-	{
-		a = power(-1, y);
-		b = power(radians, (2 * y + 1));
-		c = factorial((2 * y + 1));
-		result = result + (a * b) / c;
-	}
-	return sin(radians);
 }
